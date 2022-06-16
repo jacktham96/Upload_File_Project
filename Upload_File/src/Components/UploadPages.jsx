712dc07c0fa3d0162cd6876549ca7d96ee7354be
jacktham96 <62689233+jacktham96@users.noreply.github.com>
2022-06-17 00:48:08 +0800

    Upload Done

--- Upload_File/src/Components/UploadPages.jsx
+++ Upload_File/src/Components/UploadPages.jsx
@@ -1,19 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState , useEffect} from 'react'
 import { storage } from './firebase'
-import { ref , uploadBytes } from 'firebase/storage'
+import { ref , uploadBytes, listAll, list, getDownloadURL } from 'firebase/storage'
 
 const UploadPages = () => {
 
   const [imageUpload , setImageUpload] = useState(null)
+  const [imageList , setImageList] = useState([])
+
+  const imageListRef = ref(storage,'images/')
 
   const uploadImage = () => {
     if (imageUpload == null) return;
     const imageRef = ref(storage , `images/${imageUpload.name}`)
-    uploadBytes(imageRef , imageUpload).then(()=>{
-      alert('image uploaded')
-      console.log('test');
+    uploadBytes(imageRef , imageUpload).then((snapshot)=>{
+      getDownloadURL(snapshot.ref).then((url)=>{
+        setImageList((prev)=>[...prev,url])
+      })
     })
   }
+
+  useEffect(()=>{
+    listAll(imageListRef).then((response)=>{
+      response.items.forEach((item)=>{
+        getDownloadURL(item).then((url)=>{
+          setImageList((prev) => [...prev,url])
+        })
+      })
+    })
+  },[])
   
   return (
 
@@ -27,15 +41,16 @@ const UploadPages = () => {
                 setImageUpload(event.target.files[0])
               }}
             />
-            <i>Icon</i>
-            <button className='' onClick={uploadImage}>Upload Image</button>
+            <button className='bg-green py-4 mt-5' onClick={uploadImage}>Upload Image</button>
 
         </div>
 
 
 
-        <output className='border-2 border-white  p-20 text-left'>
-            <h1>output</h1>
+        <output className=' p-20 text-left'>
+            {imageList.map((url)=>{
+              return <img className='object-cover h-auto w-80 my-5' src={url} alt="" />
+            })}
         </output>
 
 
